722f7473eead692223070a0b84586fa47d5bc379
Ubuntu <ubuntu@ip-10-203-51-45.ec2.internal>
2013-07-10 23:09:04 +0000

    modded web.js to read from index.html attempt 1

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-	var stuff = fs.readFileSync(index.html);
+	var fs = require('fs');
+	var stuff = fs.readFileSync("index.html");
 	var strstuff = buf.toString();
 	response.send(strstuff);
 //  response.send('Hello World 2!');
